7bbca6067c6d9180d59c9a7fdfbb048a64d6892f
goddice <liucl.lsu@gmail.com>
2015-09-27 13:59:50 -0500

    shdaers

--- app/src/main/java/com/example/goddice/firstopenglproject/FirstOpenGLProjectActivity.java
+++ app/src/main/java/com/example/goddice/firstopenglproject/FirstOpenGLProjectActivity.java
@@ -1,80 +1,43 @@
 package com.example.goddice.firstopenglproject;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.pm.ConfigurationInfo;
-import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class FirstOpenGLProjectActivity extends AppCompatActivity {
-    private GLSurfaceView glSurfaceView;
-    private boolean rendererSet = false;
-
+public class FirstOpenGLProjectActivity extends Activity {
+    private MySurfaceView mGLSurfaceView;
+    static boolean threadFlag;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        glSurfaceView = new GLSurfaceView(this);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        final ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
-        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
-        if (supportsEs2)
-        {
-            glSurfaceView.setEGLContextClientVersion(2);
-            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
-            rendererSet = true;
-        }
-        else
-        {
-            Toast.makeText(this, "This device does not support OpenGL ES 2.0", Toast.LENGTH_LONG).show();
-        }
-        //setContentView(R.layout.activity_first_open_glproject);
-        setContentView(glSurfaceView);
+        mGLSurfaceView = new MySurfaceView(this);
+        setContentView(mGLSurfaceView);
+        mGLSurfaceView.requestFocus();
+        mGLSurfaceView.setFocusableInTouchMode(true);
     }
 
     @Override
-    protected void onPause()
-    {
-        super.onPause();
-        if (rendererSet)
-        {
-            glSurfaceView.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
-        if (rendererSet)
-        {
-            glSurfaceView.onResume();
-        }
+        threadFlag=true;
+        mGLSurfaceView.onResume();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_first_open_glproject, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onPause() {
+        super.onPause();
+        threadFlag=false;
+        mGLSurfaceView.onPause();
     }
 }
+
+
+
