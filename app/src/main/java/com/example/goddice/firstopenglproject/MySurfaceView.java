e62fe1bee92db7cddb7f24f64e254a2129e41778
goddice <liucl.lsu@gmail.com>
2015-09-28 23:41:07 -0500

    v3

--- app/src/main/java/com/example/goddice/firstopenglproject/MySurfaceView.java
+++ app/src/main/java/com/example/goddice/firstopenglproject/MySurfaceView.java
@@ -10,6 +10,7 @@ import android.graphics.Point;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
 import android.opengl.GLUtils;
+import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.opengl.GLES20;
@@ -48,6 +49,7 @@ class MySurfaceView extends GLSurfaceView
 	public class SceneRenderer implements GLSurfaceView.Renderer
     {
         Context mContext;
+        public final String TAG = "GVCGVC";
         public int resolution = 256;
         public float perturbance = 0.03f;
         public float textureDelte[] = {1 / (float) resolution, 1 / (float) resolution};
@@ -78,7 +80,12 @@ class MySurfaceView extends GLSurfaceView
         public int dropProgram_center_handler;
         public int dropProgram_radius_handler;
         public int dropProgram_strength_handler;
-
+        public int dropProgram_texture_handler;
+        public float xCoord;
+        public boolean inDropping = false;
+        float dropPosition[] = {0.0f, 0.0f};
+        float mRadius = 0.0f;
+        float mStrength = 0.0f;
 
         public SceneRenderer(Context context)
         {
@@ -135,13 +142,16 @@ class MySurfaceView extends GLSurfaceView
                 GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
                 GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
                 GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, resolution, resolution, 0, GLES20.GL_RGBA, GLES20.GL_FLOAT, null);
-
+                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
                 GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0, GLES20.GL_TEXTURE_2D, texture[i], 0);
 
-                GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+                //GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
                 GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
                 GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
             }
+
+
+            GLES20.glCheckFramebufferStatus(GLES20.GL_FRAMEBUFFER);
 
             // Init GL Stuff
             int [] mBuffer = new int[1];
@@ -151,6 +161,8 @@ class MySurfaceView extends GLSurfaceView
             FloatBuffer fb = ByteBuffer.allocateDirect(4 * vertexArray.length).order(ByteOrder.nativeOrder()).asFloatBuffer();
             fb.put(vertexArray);
             fb.position(0);
+            GLES20.glEnableVertexAttribArray(0);
+            GLES20.glVertexAttribPointer(0, 2, GLES20.GL_FLOAT, false, 0, 0);
             GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, vertexArray.length * 4, fb, GLES20.GL_STATIC_DRAW);
             quad = mBuffer[0];
 
@@ -179,10 +191,8 @@ class MySurfaceView extends GLSurfaceView
             dropProgram_center_handler = GLES20.glGetUniformLocation(dropProgram, "center");
             dropProgram_radius_handler = GLES20.glGetUniformLocation(dropProgram, "radius");
             dropProgram_strength_handler = GLES20.glGetUniformLocation(dropProgram, "strength");
+            dropProgram_texture_handler = GLES20.glGetUniformLocation(dropProgram, "texture");
 
-            GLES20.glUniform2fv(updateProgram_delta_handler[0], 1, textureDelte, 0);
-            GLES20.glUniform2fv(updateProgram_delta_handler[1], 1, textureDelte, 0);
-            GLES20.glUniform1f(renderProgram_perturbance_handler, perturbance);
         }
 
         public void update()
@@ -194,17 +204,19 @@ class MySurfaceView extends GLSurfaceView
         public void drawQuad()
         {
             GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, quad);
-            GLES20.glVertexAttribPointer(0, 2, GLES20.GL_FLOAT, false, 0, 0);
             GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 4);
         }
 
         public void render()
         {
+            //Log.e(TAG, "rendering");
+
+            //computeTextureBoundaries();
+
             GLES20.glViewport(0, 0, backgroundWidth, backgroundHeight);
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-            GLES20.glUseProgram(renderProgram);
-            GLES20.glEnableVertexAttribArray(0);
+            GLES20.glUseProgram(dropProgram);
 
             GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
             GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, backgroundTexture);
@@ -216,8 +228,11 @@ class MySurfaceView extends GLSurfaceView
             GLES20.glUniform2fv(renderProgram_containerRatio_handler, 1, renderProgram_containRatio, 0);
             GLES20.glUniform1i(renderProgram_samplerBackground_handler, 0);
             GLES20.glUniform1i(renderProgram_samplerRipples_handler, 1);
+//            Log.e(TAG, String.valueOf(renderProgram_perturbance_handler));
+            GLES20.glUniform1f(renderProgram_perturbance_handler, perturbance);
 
             drawQuad();
+            GLES20.glUseProgram(0);
         }
 
         public void updateTexture()
@@ -225,18 +240,20 @@ class MySurfaceView extends GLSurfaceView
             computeTextureBoundaries();
 
             GLES20.glViewport(0, 0, resolution, resolution);
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-            for (int i=0; i<2; ++i)
-            {
+            for (int i=0; i<2; ++i) {
+                GLES20.glUseProgram(updateProgram[i]);
                 GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, framebuffer[i]);
                 GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
                 GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture[1 - i]);
-                //GLES20.glUniform1i(updateProgram_texture_handler[i], 0);
-                GLES20.glUseProgram(updateProgram[i]);
-                GLES20.glEnableVertexAttribArray(0);
+                GLES20.glUniform1i(updateProgram_texture_handler[i], 0);
+                GLES20.glUniform2f(updateProgram_delta_handler[i], textureDelte[0], textureDelte[1]);
 
                 drawQuad();
+                GLES20.glUseProgram(0);
             }
+
 
             GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
         }
@@ -255,32 +272,91 @@ class MySurfaceView extends GLSurfaceView
 
         public void dropAtFinger(MotionEvent e, float radius, float strength)
         {
+            //computeTextureBoundaries();
+            //inDropping = true;
+
             int longestSide = Math.max(backgroundHeight, backgroundWidth);
-            float dropPosition[] = {(2 * e.getX() - (float)backgroundWidth) / (float)longestSide, ((float)backgroundHeight - 2 * e.getY()) / (float) longestSide};
-            GLES20.glViewport(0, 0, resolution, resolution);
+            dropPosition[0] = (2 * e.getX() - (float)backgroundWidth) / (float)longestSide;
+            dropPosition[1] = ((float)backgroundHeight - 2 * e.getY()) / (float) longestSide;
+            mRadius = radius;
+            mStrength = strength;
 
-            GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, framebuffer[0]);
 
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture[1]);
+            //GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, framebuffer[0]);
+            //GLES20.glViewport(0, 0, resolution, resolution);
 
-            GLES20.glUseProgram(dropProgram);
-            GLES20.glEnableVertexAttribArray(0);
-            GLES20.glUniform2fv(dropProgram_center_handler, 1, dropPosition, 0);
-            GLES20.glUniform1f(dropProgram_radius_handler, radius);
-            GLES20.glUniform1f(dropProgram_strength_handler, strength);
+//            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+//            GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+//
+//            GLES20.glUseProgram(dropProgram);
+//
+//            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+//            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, backgroundTexture);
+//            GLES20.glUniform1i(dropProgram_texture_handler, 0);
+//
+//            GLES20.glUniform2f(dropProgram_center_handler, dropPosition[0], dropPosition[1]);
+//            Log.e(TAG, "radius: " + String.valueOf(radius));
+//            Log.e(TAG, "strength: " + String.valueOf(strength));
+//            Log.e(TAG, "dropProgram_radius_handler: " + String.valueOf(dropProgram_radius_handler));
+//            Log.e(TAG, "dropProgram_strength_handler: " + String.valueOf(dropProgram_strength_handler));
+//            Log.e(TAG, "dropProgram_center_handler: " + String.valueOf(dropProgram_center_handler));
+//            xCoord = dropPosition[0];
+//            GLES20.glUniform1f(dropProgram_radius_handler, radius);
+//            GLES20.glUniform1f(dropProgram_strength_handler, strength);
+//
+//            drawQuad();
+//            GLES20.glUseProgram(0);
+//
+//
+//            int t = framebuffer[0]; framebuffer[0] = framebuffer[1]; framebuffer[1] = t;
+//            t = texture[0]; texture[0] = texture[1]; texture[1] = t;
 
-            drawQuad();
-
-            int t = framebuffer[0]; framebuffer[0] = framebuffer[1]; framebuffer[1] = t;
-            t = texture[0]; texture[0] = texture[1]; texture[1] = t;
-
-            GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+            //GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+            //GLES20.glUseProgram(0);
+//            inDropping = true;
         }
 
         public void onDrawFrame(GL10 gl) 
         {
+            if (mStrength > 0.0001)
+            {
+                GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, framebuffer[0]);
+                GLES20.glViewport(0, 0, resolution, resolution);
+
+                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+                GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+                GLES20.glUseProgram(dropProgram);
+
+                GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture[1]);
+                GLES20.glUniform1i(dropProgram_texture_handler, 0);
+
+                GLES20.glUniform2f(dropProgram_center_handler, dropPosition[0], dropPosition[1]);
+
+                GLES20.glUniform1f(dropProgram_radius_handler, mRadius);
+                GLES20.glUniform1f(dropProgram_strength_handler, mStrength);
+                if (mRadius > 0) Log.e(TAG, String.valueOf(mRadius));
+                if (mStrength > 0) Log.e(TAG, String.valueOf(mStrength));
+
+                drawQuad();
+                GLES20.glUseProgram(0);
+
+
+                int t = framebuffer[0]; framebuffer[0] = framebuffer[1]; framebuffer[1] = t;
+                t = texture[0]; texture[0] = texture[1]; texture[1] = t;
+
+                GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+            }
+            //GLES20.glUseProgram(0);
+            //inDropping = true;
+
+
             update();
+            dropPosition[0] = 0.0f;
+            dropPosition[1] = 0.0f;
+            mRadius = 0.0f;
+            mStrength = 0.0f;
         }  
 
         public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -289,7 +365,7 @@ class MySurfaceView extends GLSurfaceView
 
         public void onSurfaceCreated(GL10 gl, EGLConfig config)
         {
-            GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             try
             {
                 initTextures();
